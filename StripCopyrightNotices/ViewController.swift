76321c727138ab142f319cbef77055fef923d37b
Joseph Wardell <jaywardell@mac.com>
2020-05-15 19:46:47 -0400

    Initial Commit

---
+++ StripCopyrightNotices/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StripCopyrightNotices
+//
+//  Created by Joseph A. Wardell on 5/15/20.
+//  Copyright © 2020 Joseph A. Wardell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
